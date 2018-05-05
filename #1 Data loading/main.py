5abab899a657fcb8a7dc5aeaaff81d4a7b57d293
aakashvarma18 <aakashvarma18@gmail.com>
2018-05-05 22:51:03 +0530

    pandas head

--- #1 Data loading/main.py
+++ #1 Data loading/main.py
@@ -2,13 +2,14 @@
 
 import csv
 import numpy as np
+import pandas as pd
 filename = 'diabetes.csv'
 raw_data = open(filename, 'r')
 reader = csv.reader(raw_data, delimiter = ',', quoting = csv.QUOTE_NONE)
 x = list(reader)
-data = np.array(x)
-print(data.shape)
-
+df = pd.DataFrame(x)
+print(df.shape)
+print(df.head())
 
 
 
