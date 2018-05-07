ce2657d72fcafbaa30fc1156f7d07b6f19211c4a
aakashvarma18 <aakashvarma18@gmail.com>
2018-05-07 12:49:07 +0530

    changes

---
+++ #0 Data visualisation/main.py
@@ -0,0 +1,27 @@
+import numpy as np 
+import pandas as pd
+import matplotlib.pyplot as plt
+
+x = np.linspace(0, 2*np.pi, 400)
+y = np.sin(x**2)
+
+# Subplotting
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
