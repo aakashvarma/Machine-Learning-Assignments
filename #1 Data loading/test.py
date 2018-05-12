2c78ed4d15c1443b27b301caa321a126e250a93d
aakashvarma18 <aakashvarma18@gmail.com>
2018-05-12 22:00:06 +0530

    Test file added

---
+++ #1 Data loading/test.py
@@ -0,0 +1,46 @@
+import matplotlib.pyplot as plt
+from matplotlib import cm as cm
+import matplotlib
+import numpy as np
+import pandas as pd
+from pandas import read_csv
+
+# vegetables = ["cucumber", "tomato", "lettuce", "asparagus",
+#               "potato", "wheat", "barley"]
+# farmers = ["Farmer Joe", "Upland Bros.", "Smith Gardening",
+#            "Agrifun", "Organiculture", "BioGoods Ltd.", "Cornylee Corp."]
+
+# corr_matrix = np.random.randn(7,7)
+# print (corr_matrix)
+
+# def heat_map(cm):
+#     fig, ax = plt.subplots()
+#     ax.imshow(cm)
+
+#     ax.set_xticks(np.arange(len(farmers)))
+#     ax.set_yticks(np.arange(len(vegetables)))
+    
+#     ax.set_xticklabels(farmers, rotation =45, ha="right", rotation_mode="anchor")
+#     ax.set_yticklabels(vegetables)
+#     plt.show()
+
+
+# heat_map(corr_matrix)
+
+df = read_csv()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
