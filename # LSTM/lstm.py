bb41b54211eefa5e084da5e45257f826b85c8ebe
aakashvarma18 <aakashvarma18@gmail.com>
2018-05-23 00:28:33 +0530

    keras model

--- # LSTM/lstm.py
+++ # LSTM/lstm.py
@@ -1,13 +1,14 @@
 # Simple Neural Networks
 
+from keras.models import Sequential
+from keras.layers import Dense
+import numpy as np
 
+np.random.seed(7)
+dataset = np.loadtxt("pima-indians-diabetes.csv", delimiter=",")
 
-
-
-
-
-
-
+X = dataset[:,0:8]
+Y = dataset[:,8]
 
 
 
