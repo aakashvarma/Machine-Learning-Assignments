30a3eac926354cce2b428285b41c804964191196
aakashvarma18 <aakashvarma18@gmail.com>
2018-05-23 01:53:10 +0530

    Compile code added

--- # LSTM/lstm.py
+++ # LSTM/lstm.py
@@ -10,9 +10,12 @@ dataset = np.loadtxt("pima-indians-diabetes.csv", delimiter=",")
 X = dataset[:,0:8]
 Y = dataset[:,8]
 
+model = Sequential()
+model.add(Dense(12, input_dim = 8, activation = 'relu'))
+model.add(Dense(8, activation = 'relu'))
+model.add(Dense(1, activation = 'sigmoid'))
 
-
-
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 
 
